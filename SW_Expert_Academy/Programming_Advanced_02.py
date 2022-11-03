bdf21c048e3c4dae431e001195c873fad1286bcb
heohyoyeong <heohyoyeong@gmail.com>
2022-11-03 10:54:57 +0900

    5189. [파이썬 S/W 문제해결 구현] 2일차 - 전자카트

--- SW_Expert_Academy/Programming_Advanced_02.py
+++ SW_Expert_Academy/Programming_Advanced_02.py
@@ -22,3 +22,27 @@ for test_case in range(1, T + 1):
     result = []
     result = dfs(0, 0, 0)
     print(f"#{test_case} {min(result)}")
+
+
+### problem 2
+
+
+from itertools import permutations
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    n = int(input())
+    array = [list(map(int, input().split())) for _ in range(n)]
+    total = [z for z in range(2,n+1)]
+    perm = list(permutations(total,n-1))
+    result = []
+    for i in range(len(perm)):
+        test = list(perm[i])
+        test.append(1)
+        test.insert(0,1)
+        sum = 0
+        for j in range(len(test)-1):
+            sum += array[test[j]-1][test[j+1]-1]
+        result.append(sum)
+    print(f"#{test_case} {min(result)}")
